0e4442881c90fba6d1a71928a1bf4376c8a6947e
romelgomez <bmxandcode@gmail.com>
2024-09-25 20:55:11 -0500

    test: use netlify adapter - take 001

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
 import clerk from '@clerk/astro';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [clerk()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
